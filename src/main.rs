7cd1e9620d7d21793c002679aa6d6d28d8ba09a5
Rasmus Christensen <r@musn.eu>
2023-08-27 13:35:35 +0200

    working candidate

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
 use rusty_mining::mining::{ run_profile, config::read_profiles_from_config };
-use std::{thread, time::Duration};
+use std::{thread, time::Duration, thread::JoinHandle };
 
 fn main() {
     println!("Rusty Mining");
     let miner_config = read_profiles_from_config(false);
+    let mut join_handles: Vec<JoinHandle<()>>= vec![];
+    let mut active_handles = 0;
     miner_config.profiles.iter().for_each(|profile| {
         let local_thread_profile = profile.clone();
         let handle = thread::spawn(|| {
             run_profile(local_thread_profile);
         });
-        while handle.is_finished() != true {
-            thread::sleep(Duration::from_secs(10));
-        }
+        join_handles.push(handle);
+        active_handles += 1;
     });
+    let mut active_handles_changed = false;
+    while active_handles > 0 {
+        for h in join_handles.iter() {
+            if h.is_finished() {
+                active_handles -= 1;
+                active_handles_changed = true;
+            }
+        }
+        if active_handles_changed {
+            println!("Active CPU threads running miners: {}", active_handles);
+            active_handles_changed = false;
+        }
+        thread::sleep(Duration::from_secs(1));
+    }
 }
--- src/mining/mod.rs
+++ src/mining/mod.rs
@@ -1,6 +1,5 @@
 use self::config::{Profile, print_profile};
 use std::process::Command;
-use std::io::{self, Write};
 
 pub mod config;
 
@@ -9,37 +8,25 @@ pub fn run_profile(profile: Profile) {
     print!("Executing command: ");
     print_profile(&profile);
     if profile.additionals.len() > 0 {
-        let child = Command::new("cmd")
+        let status = Command::new("cmd")
             .args(["/C", format!("{}\\{}", profile.path, profile.file).as_str()])
             .args(["-a", profile.algorithm.as_str()])
             .args(["-o", profile.url.as_str()])
             .args(["-u", format!("{}:{}.{}", profile.coin, profile.wallet_address, profile.worker_name).as_str()])
             .args(["-d", profile.devices.as_str()])
             .args(["", profile.additionals.as_str()])
-            .spawn()
+            .status()
             .expect("failed to execute process...");
-        //wait_for_child(child);
+        println!("CPU thread finished with: {status}");
     } else {
-        let child = Command::new("cmd")
+        let status = Command::new("cmd")
             .args(["/C", format!("{}\\{}", profile.path, profile.file).as_str()])
             .args(["-a", profile.algorithm.as_str()])
             .args(["-o", profile.url.as_str()])
             .args(["-u", format!("{}:{}.{}", profile.coin, profile.wallet_address, profile.worker_name).as_str()])
             .args(["-d", profile.devices.as_str()])
-            .spawn()
+            .status()
             .expect("failed to execute process...");
-        //wait_for_child(child);
+        println!("CPU thread running miner [with profile: {}] finished with: {}", profile.profile_name, status);
     }
-}
-
-fn wait_for_child(child: std::process::Child) {
-    match child.wait_with_output() {
-        Ok(out) => {
-            println!("Process exited with success.");
-            io::stdout().write_all(&out.stdout).unwrap();
-        },
-        Err(err) => {
-            println!("Process exited with errors: {}", &err.to_string());
-        },
-    };
 }
